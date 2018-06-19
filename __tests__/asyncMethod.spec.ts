80aeb00cf7acb7c1db466fd21e8b67ae033a6b76
D-Y <dmiyer@gmail.com>
2018-06-19 11:39:51 +0300

    Added failing test

--- __tests__/asyncMethod.spec.ts
+++ __tests__/asyncMethod.spec.ts
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { renderToString } from 'react-dom/server'
-import { createApi, IApi } from './helpers';
+import { createApi } from './helpers';
 import { asyncMethod, createFetcher, Fetcher } from '../index';
+import * as Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 class Test extends React.Component<{ fetcher: Fetcher<string>}, {}> {
     render() {
@@ -45,14 +49,21 @@ it('render error', async () => {
 it('render data', async () => {
     const api = createApi<string>();
     const fetcher = createFetcher(api.fetch);
+    const component = Enzyme.mount(React.createElement(Test, {fetcher: fetcher}));
     api.resolve('content');
-    try {
-        fetcher.get();
-        expect(false).toBeTruthy()
-    } catch (e) {
-        expect(true).toBeTruthy();
-    }
     await api.defer;
-    const component = renderToString(React.createElement(Test, { fetcher }))
-    expect(component).toMatchSnapshot();
+    expect(component.html()).toMatchSnapshot();
+});
+
+it('render multiple components data', async () => {
+    const api = createApi<string>();
+    const fetcher = createFetcher(api.fetch);
+    const components = [1, 2, 3].map(() => {
+        return Enzyme.mount(React.createElement(Test, {fetcher}));
+    });
+    api.resolve('content');
+    await api.defer;
+    components.map((component) => {
+        expect(component.html()).toMatchSnapshot();
+    });
 });
