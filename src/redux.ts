3b53d6da012ce1cb68199e6a8b6224e58739de54
Efremov Alexey <lexich121@gmail.com>
2018-07-31 11:51:56 +0300

    Fix: initRedux API

--- src/redux.ts
+++ src/redux.ts
@@ -3,34 +3,31 @@ import { MiddlewareAPI, Dispatch, IDataItem, IActionFetch, IFetcherOption } from
 import { IOptionStore, createReducer, IOptionReducer } from './reduxReducer';
 export { MiddlewareAPI, Dispatch, IDataItem, IActionFetch, Fetcher, TFetcherFn, IFetcherOption };
 
-export function initRedux<State>() {
-	function createReduxFetcher(opt: IOptionStore) {
-		let api: MiddlewareAPI;
+export function initRedux<State>(opt: IOptionStore) {
+  let api: MiddlewareAPI;
 
-		function use(aApi: MiddlewareAPI): void {
-			api = aApi;
-		}
+  function use(aApi: MiddlewareAPI): void {
+    api = aApi;
+  }
 
-		const store: MiddlewareAPI = {
-			getState() {
-				return api.getState();
-			},
-			dispatch(action: any) {
-				return api.dispatch(action);
-			},
-		};
+  const store: MiddlewareAPI = {
+    getState() {
+      return api.getState();
+    },
+    dispatch(action: any) {
+      return api.dispatch(action);
+    },
+  };
 
-		// reducer initialize after create call
-		let reducer: (state: State, act: IActionFetch) => State;
+  // reducer initialize after create call
+  let reducer: (state: State, act: IActionFetch) => State;
 
-		// it's sync callback which call after create call
-		function createStore(opt: IOptionReducer<any>): MiddlewareAPI {
-			reducer = createReducer<State>(opt);
-			return store;
-		}
+  // it's sync callback which call after create call
+  function createStore(opt: IOptionReducer<any>): MiddlewareAPI {
+    reducer = createReducer<State>(opt);
+    return store;
+  }
 
-		const fn = create({ ...opt, createStore });
-		return { use, reducer, createFetcher: fn };
-	}
-	return createReduxFetcher;
+  const fn = create({ ...opt, createStore });
+  return { use, reducer, createFetcher: fn };
 }
